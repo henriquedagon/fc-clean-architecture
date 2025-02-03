ce10900ca8a235515fd0c2e772a88cec04a9dd8d
henriquegon <henrique.goncalves@supersim.com.br>
2025-02-03 12:03:39 -0300

    feat: add product e2e test

--- src/usecase/product/list/list.product.usecase.ts
+++ src/usecase/product/list/list.product.usecase.ts
@@ -1,6 +1,7 @@
 import ProductRepositoryInterface from "../../../domain/product/repository/product-repository.interface";
 import {InputListProductDto, OutputListProductDto,} from "./list.product.dto";
 import ProductInterface from "../../../domain/product/entity/product.interface";
+import Product from "../../../domain/product/entity/product";
 
 export default class ListProductUseCase {
     private productRepository: ProductRepositoryInterface<ProductInterface>;
@@ -14,6 +15,18 @@ export default class ListProductUseCase {
     ): Promise<OutputListProductDto> {
 
         const products = await this.productRepository.findAll();
-        return { products };
+        return OutputMapper.toOutput(products);
+    }
+}
+
+class OutputMapper {
+    static toOutput(product: ProductInterface[]): OutputListProductDto {
+        return {
+            products: product.map((product) => ({
+                id: product.id,
+                name: product.name,
+                price: product.price,
+            })),
+        };
     }
 }
